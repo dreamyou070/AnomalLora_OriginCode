76818283d8505304c6565edec0904190cd10ce15
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-19 21:56:15 +0900

    third file

--- script/train_20240219.sh
+++ script/train_20240219.sh
@@ -5,11 +5,11 @@ port_number=50001
 obj_name='carrot'
 trigger_word='carrot'
 bench_mark='MVTec3D-AD'
-
+save_folder_name="1_3_normal_sample_anormal_sample_background_masked_sample_attn_loss_dist_loss_map_loss"
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train_20240219.py \
  --log_with wandb \
- --output_dir "../../result/${bench_mark}/${obj_name}/test" \
+ --output_dir "../../result/${bench_mark}/${obj_name}/${save_folder_name}" \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" --beta_scale_factor 0.8 \
  --bgrm_test \
@@ -22,4 +22,9 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --start_epoch 0 --max_train_epochs 30 \
- --do_object_detection
+ --do_normal_sample \
+ --do_anomal_sample \
+ --do_background_masked_sample \
+ --do_attn_loss \
+ --do_dist_loss \
+ --do_map_loss
