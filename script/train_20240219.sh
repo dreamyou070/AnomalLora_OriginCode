3b1a32bc744c84fde3b939c73e81696071547675
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-19 14:51:44 +0900

    Update train_20240219.sh

--- script/train_20240219.sh
+++ script/train_20240219.sh
@@ -16,7 +16,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" --beta_scale_factor 0.8 \
  --trigger_word "${trigger_word}" --obj_name "${obj_name}" --train_unet --train_text_encoder --d_dim 320 --latent_res 64 \
- --use_position_embedder --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' \
+ --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
- --start_epoch 0 --max_train_epochs 300 \
+ --start_epoch 0 --max_train_epochs 30 \
  --do_attn_loss
