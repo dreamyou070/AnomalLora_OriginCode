bfb44ece776537d9d86f56b955f6dbb366f22f9b
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-19 15:07:31 +0900

    train with small data

--- script/train.sh
+++ script/train.sh
@@ -10,7 +10,7 @@ anomal_source_path="../../../MyData/anomal_source"
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
- --output_dir "../../result/${bench_mark}/${obj_name}/0_origin_code_test_20240219" \
+ --output_dir "../../result/${bench_mark}/${obj_name}/0_origin_code_test_20240219_small_data" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 30 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --beta_scale_factor 0.8 --anomal_source_path "${anomal_source_path}" --anomal_only_on_object \
@@ -18,4 +18,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --use_position_embedder --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' --d_dim 320 --latent_res 64 \
  --do_dist_loss --dist_loss_weight 1.0 --do_down_dim_mahal_loss --down_dim 100 \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
- --do_map_loss
+ --do_map_loss --use_small_anomal
