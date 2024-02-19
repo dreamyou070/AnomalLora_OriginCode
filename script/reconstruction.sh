e1029c8f88727fe0d189eff22018559778f1e4bd
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-20 00:29:24 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -3,7 +3,7 @@
 port_number=53311
 obj_name='carrot'
 caption='carrot'
-folder_name="0_2_student_model_without_attn_loss"
+folder_name="1_5_anormal_sample_background_masked_sample_attn_loss_dist_loss_map_loss_only_zero_timestep"
 bench_mark="MVTec3D-AD"
 position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 
