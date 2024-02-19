c8789e767d46d9be49de413449e03598b3eb00fa
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-19 22:23:13 +0900

    Update train_20240219.sh

--- script/train_20240219.sh
+++ script/train_20240219.sh
@@ -8,6 +8,7 @@ obj_name='carrot'
 trigger_word='carrot'
 bench_mark='MVTec3D-AD'
 save_folder_name="1_3_normal_sample_anormal_sample_background_masked_sample_attn_loss_dist_loss_map_loss"
+
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train_20240219.py \
  --log_with wandb \
@@ -25,6 +26,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --start_epoch 0 --max_train_epochs 30 \
  --do_normal_sample \
+ --do_anomal_sample \
  --do_attn_loss \
  --do_dist_loss \
  --do_map_loss
