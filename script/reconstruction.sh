263169f14d98345fc5745363211a800c02319c67
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-21 07:01:23 +0900

    recon 2

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -15,6 +15,6 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}/${obj_name}/test" \
  --obj_name "${obj_name}" --prompt "${caption}" \
  --latent_res 64 --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2',
-                                    'up_blocks_1_attentions_2_transformer_blocks_0_attn2']" \
+                                    'down_blocks_1_attentions_1_transformer_blocks_0_attn2']" \
  --d_dim 320 --use_position_embedder --position_embedding_layer ${position_embedding_layer} \
  --threds [0.5] --do_normalized_score
