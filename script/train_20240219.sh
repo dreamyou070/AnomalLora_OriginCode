699597328b670184875227f40623ceacf0130222
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-20 11:52:45 +0900

    Update train_20240219.sh

--- script/train_20240219.sh
+++ script/train_20240219.sh
@@ -5,7 +5,7 @@ port_number=50321
 obj_name='carrot'
 trigger_word='carrot'
 bench_mark='MVTec3D-AD'
-save_folder_name="do_normal_sample_do_anomal_sample_attn_loss_normalized_score_map_loss"
+save_folder_name="do_normal_sample_do_anomal_sample_dist_loss_attn_loss_normalized_score"
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
  --main_process_port $port_number ../train_20240219.py \
@@ -24,5 +24,5 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --start_epoch 0 --max_train_epochs 30 \
  --do_normal_sample --do_anomal_sample \
- --do_attn_loss --do_normalized_score \
- --do_map_loss
+ --do_dist_loss \
+ --do_attn_loss --do_normalized_score
