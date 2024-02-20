a3d6f652693c204c556fd05e643c7e4742d620ec
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-20 14:44:42 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -3,14 +3,15 @@
 port_number=53153
 obj_name='carrot'
 caption='carrot'
-folder_name="1_5_anormal_sample_background_masked_sample_attn_loss_map_loss_only_zero_timestep"
+sub_folder="sub_3_background_masked_sample_anomal_sample"
+folder_name="attn_loss_map_loss"
 bench_mark="MVTec3D-AD"
 position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_dim 64 --network_alpha 4 --network_folder "../../result/${bench_mark}/${obj_name}/${folder_name}/models" \
+ --network_dim 64 --network_alpha 4 --network_folder "../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}/models" \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}/${obj_name}/test" \
  --obj_name "${obj_name}" --prompt "${caption}" \
  --latent_res 64 --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
