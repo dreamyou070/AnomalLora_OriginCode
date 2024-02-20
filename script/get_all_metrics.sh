c12320874f3679ef8a6e3b90adc9df1ed02425a6
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-20 15:14:16 +0900

    Update get_all_metrics.sh

--- script/get_all_metrics.sh
+++ script/get_all_metrics.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
+bench_mark="MVTec3D-AD"
 class_name="carrot"
 second_folder_name="sub_3_background_masked_sample_anomal_sample"
 folder="attn_loss_map_loss"
-bench_mark="MVTec3D-AD"
+
 
 
 python ../evaluation/get_all_metrics.py \
   --class_name ${class_name} \
-  --second_folder_name ${second_folder_name} \
-  --base_save_dir "../../result/${bench_mark}/${class_name}/${second_folder_name}"
+  --base_save_dir "../../result/${bench_mark}/${class_name}/${second_folder_name}/${folder}"
