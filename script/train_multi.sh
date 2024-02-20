9283aa7464877506f9c01595429e668b6c548a6a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-20 14:59:26 +0900

    train two layers

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -18,7 +18,8 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --bgrm_test \
  --trigger_word "${trigger_word}" --obj_name "${obj_name}" --train_unet --train_text_encoder --d_dim 320 --latent_res 64 \
  --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' \
- --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
+ --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2',
+                    'up_blocks_2_attentions_2_transformer_blocks_0_attn2']" \
  --start_epoch 0 --max_train_epochs 30 \
  --do_anomal_sample \
  --do_dist_loss \
