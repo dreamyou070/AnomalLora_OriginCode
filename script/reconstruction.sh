24262fd12b4ff457768a48494de9ed912e009e35
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-21 06:59:10 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -17,4 +17,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --latent_res 64 --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2',
                                     'up_blocks_1_attentions_2_transformer_blocks_0_attn2']" \
  --d_dim 320 --use_position_embedder --position_embedding_layer ${position_embedding_layer} \
- --threds [0.5] --do_normalized_score --single_layer
+ --threds [0.5] --do_normalized_score
