72796afd8c38ece178e4095c5cd442cf288a43bf
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-20 11:48:30 +0900

    Update train_20240219.sh

--- script/train_20240219.sh
+++ script/train_20240219.sh
@@ -1,13 +1,13 @@
 # !/bin/bash
 
-port_number=50023
+port_number=50310
 
 obj_name='carrot'
 trigger_word='carrot'
 bench_mark='MVTec3D-AD'
-save_folder_name="dist_loss_attn_loss_normalized_score"
+save_folder_name="do_normal_sample_do_anomal_sample_dist_loss_attn_loss_normalized_score_map_loss"
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
  --main_process_port $port_number ../train_20240219.py \
  --log_with wandb \
  --output_dir "../../result/${bench_mark}/${obj_name}/${save_folder_name}" \
@@ -23,6 +23,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config \
  --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --start_epoch 0 --max_train_epochs 30 \
- --do_anomal_sample \
+ --do_normal_sample --do_anomal_sample \
  --do_dist_loss \
- --do_attn_loss --do_normalized_score
+ --do_attn_loss --do_normalized_score \
+ --do_map_loss
