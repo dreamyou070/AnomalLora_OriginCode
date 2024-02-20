2905c13329695ef6002971f30d82bd35baf7df11
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-21 07:18:48 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -1,28 +1,28 @@
 # !/bin/bash
 
-port_number=50003
-
+port_number=50002
+pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
 obj_name='cookie'
 trigger_word='cookie'
 bench_mark='MVTec3D-AD'
-save_folder_name="sub_3_background_masked_sample_anomal_sample"
-folder="attn_loss_normalized_score_map_loss"
+sub_folder="sub_3_background_masked_sample_anomal_sampl"
+folder_name="attn_loss_normalized_score_map_loss_train_code"
+output_dir="../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}"
+
+
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py \
  --log_with wandb \
- --output_dir "../../result/${bench_mark}/${obj_name}/${save_folder_name}/${folder}" \
- --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
+ --output_dir ${output_dir} \
+ --pretrained_model_name_or_path "${pretrained_model_name_or_path}" \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" --beta_scale_factor 0.8 \
- --bgrm_test \
- --anomal_source_path "../../../MyData/anomal_source" \
  --anomal_only_on_object \
- --trigger_word "${trigger_word}" \
- --obj_name "${obj_name}" \
- --train_unet --train_text_encoder \
- --d_dim 320 --latent_res 64 \
+ --bgrm_test \
+ --trigger_word "${trigger_word}" --obj_name "${obj_name}" \
+ --train_unet --train_text_encoder --d_dim 320 --latent_res 64 \
  --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' \
- --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
+ --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --start_epoch 0 --max_train_epochs 30 \
  --do_anomal_sample --do_background_masked_sample \
  --do_attn_loss --do_normalized_score \
