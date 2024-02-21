58d281a554e43cf489f8a73341cd4cfbac619e56
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-21 16:40:25 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -1,12 +1,12 @@
 # !/bin/bash
 
-port_number=59382
+port_number=59383
 pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
 obj_name='carrot'
 trigger_word='carrot'
 bench_mark='MVTec3D-AD'
 
-sub_folder="sub_3_background_masked_sample_anomal_sample"
+sub_folder="sub_3_background_masked_sample_anomal_sample_up_32_16"
 folder_name="attn_loss_original_normalized_score_map_loss"
 output_dir="../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}"
 
@@ -22,7 +22,8 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --trigger_word "${trigger_word}" --obj_name "${obj_name}" \
  --train_unet --train_text_encoder --d_dim 320 --latent_res 64 \
  --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' \
- --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
+ --trg_layer_list "['up_blocks_2_attentions_2_transformer_blocks_0_attn2',
+                    'up_blocks_1_attentions_2_transformer_blocks_0_attn2']" \
  --start_epoch 0 --max_train_epochs 30 \
  --do_anomal_sample --do_background_masked_sample \
  --do_attn_loss --do_normalized_score --original_normalized_score \
