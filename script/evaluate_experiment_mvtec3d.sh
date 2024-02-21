ff98c2cdfabcde02fff7bf564953ae5f0b915e79
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-22 08:04:50 +0900

    Update evaluate_experiment_mvtec3d.sh

--- script/evaluate_experiment_mvtec3d.sh
+++ script/evaluate_experiment_mvtec3d.sh
@@ -5,7 +5,7 @@ bench_mark="MVTec3D-AD"
 dataset_dir="../../../MyData/anomaly_detection/${bench_mark}"
 
 sub_folder="sub_3_background_masked_sample_anomal_sample"
-folder_name="attn_loss_original_normalized_score_map_loss_noise_predicting_task_loss"
+folder_name="attn_loss_original_normalized_score_map_loss_normalizing_mahal_feat"
 
 output_dir="metrics"
 
