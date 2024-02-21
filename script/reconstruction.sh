7175efaf0f2eb86de8afa0fdb3edc4bcd15e8c3f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-21 09:48:17 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -3,8 +3,8 @@
 port_number=51111
 obj_name='carrot'
 caption='carrot'
-sub_folder="sub_3_background_masked_sample_anomal_sample_train_code_check_again"
-folder_name="attn_loss_normalized_score_map_loss_train_code_without_normalized"
+sub_folder="train_code_check"
+folder_name="train_code_check"
 bench_mark="MVTec3D-AD"
 position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 
