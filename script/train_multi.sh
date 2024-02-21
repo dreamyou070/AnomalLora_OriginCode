d29d43cf3d86dc5e9ffae5705dabd95dfe239aea
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-21 23:22:45 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -1,17 +1,17 @@
 # !/bin/bash
 
-port_number=50005
+port_number=50006
 pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
 obj_name='carrot'
 trigger_word='carrot'
 bench_mark='MVTec3D-AD'
 
 sub_folder="sub_3_background_masked_sample_anomal_sample"
-folder_name="attn_loss_original_normalized_score_map_loss_object_dist_loss"
+folder_name="attn_loss_original_normalized_score_map_loss_noise_predicting_task_loss"
 output_dir="../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}"
-network_weights="../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}/models/epoch-000009.safetensors"
+network_weights="../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}/models/epoch-000005.safetensors"
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_config \
  --main_process_port $port_number ../train_multi.py \
  --log_with wandb \
  --output_dir ${output_dir} \
@@ -26,8 +26,8 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config \
  --network_weights "${network_weights}" \
  --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
- --start_epoch 9 --max_train_epochs 30 \
- --do_dist_loss --normal_mahal_test \
+ --start_epoch 5 --max_train_epochs 30 \
  --do_anomal_sample --do_background_masked_sample \
  --do_attn_loss --do_normalized_score --original_normalized_score \
- --do_map_loss
+ --do_map_loss \
+ --test_noise_predicting_task_loss
