bfe8b3c0200d717af31504b1f8f967c037045402
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-21 20:14:41 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -4,7 +4,7 @@ port_number=51141
 obj_name='carrot'
 caption='carrot'
 sub_folder="sub_3_background_masked_sample_anomal_sample"
-folder_name="attn_loss_original_normalized_score_map_loss"
+folder_name="sub_3_background_masked_sample_anomal_sample_up_32_16"
 bench_mark="MVTec3D-AD"
 position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 
@@ -14,6 +14,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --network_dim 64 --network_alpha 4 --network_folder "../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}/models" \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}/${obj_name}/test" \
  --obj_name "${obj_name}" --prompt "${caption}" \
- --latent_res 64 --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
+ --latent_res 64 --trg_layer_list "['up_blocks_2_attentions_2_transformer_blocks_0_attn2',
+                                    'up_blocks_1_attentions_2_transformer_blocks_0_attn2']" \
  --d_dim 320 --use_position_embedder --position_embedding_layer ${position_embedding_layer} \
  --threds [0.5]
