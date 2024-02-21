9b818cd8bec307752c062425f7d36fea545962d7
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-21 21:26:20 +0900

    Update background_remove.py

--- data/background_remove.py
+++ data/background_remove.py
@@ -50,6 +50,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--base_folder', type=str,
                         default=r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD')
-    parser.add_argument('--trg_cat', type=str, default='dowel')
+    parser.add_argument('--trg_cat', type=str, default='foam')
     args = parser.parse_args()
     main(args)
