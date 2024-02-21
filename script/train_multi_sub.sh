c4d5c1b9be5844697f45d06e458626e0413d5b24
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-21 16:38:03 +0900

    multi code

--- script/train_multi_sub.sh
+++ script/train_multi_sub.sh
@@ -1,15 +1,18 @@
-# !/bin/bash
 
-port_number=59382
+#
+
+ #
+ # !/bin/bash
+#
+port_number=53202
 pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
 obj_name='carrot'
 trigger_word='carrot'
 bench_mark='MVTec3D-AD'
-
-sub_folder="sub_3_background_masked_sample_anomal_sample"
-folder_name="attn_loss_original_normalized_score_map_loss"
+sub_folder="sub_3_background_masked_sample_anomal_sample_up_32"
+folder_name="attn_loss_normalized_map_loss_not_dist_loss"
 output_dir="../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}"
-
+network_weights="../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}/epoch-000004.safetensors"
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train_multi.py \
  --log_with wandb \
@@ -17,6 +20,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --pretrained_model_name_or_path "${pretrained_model_name_or_path}" \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" --beta_scale_factor 0.8 \
  --anomal_source_path "../../../MyData/anomal_source" \
+ --network_weights "${network_weights}" \
  --anomal_only_on_object \
  --bgrm_test \
  --trigger_word "${trigger_word}" --obj_name "${obj_name}" \
@@ -24,6 +28,8 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --start_epoch 0 --max_train_epochs 30 \
- --do_anomal_sample --do_background_masked_sample \
- --do_attn_loss --do_normalized_score --original_normalized_score \
- --do_map_loss
+ --do_anomal_sample \
+ --do_background_masked_sample \
+ --do_attn_loss --do_normalized_score \
+ --do_map_loss \
+ --original_normalized_score
