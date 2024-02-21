64b2ebbaafae6de53656d2a32f2b9b73a4a8d9cf
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-22 08:14:52 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -1,16 +1,17 @@
 # !/bin/bash
 
-port_number=59854
+port_number=59898
 pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
 obj_name='dowel'
 trigger_word='dowel'
 bench_mark='MVTec3D-AD'
 
+
 sub_folder="sub_3_background_masked_sample_anomal_sample_up_16_32_64"
-folder_name="attn_loss_original_normalized_score_map_loss_noise_predicting_task_loss"
+folder_name="attn_loss_original_normalized_score_map_loss_dist_loss_normalized"
 output_dir="../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}"
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train_multi.py \
  --log_with wandb \
  --output_dir ${output_dir} \
@@ -26,8 +27,8 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_1_attentions_2_transformer_blocks_0_attn2']" \
- --start_epoch 5 --max_train_epochs 30 \
+ --start_epoch 0 --max_train_epochs 30 \
  --do_anomal_sample --do_background_masked_sample \
+ --do_dist_loss --feature_normalize_on_mahal_dist  \
  --do_attn_loss --do_normalized_score --original_normalized_score \
- --do_map_loss \
- --test_noise_predicting_task_loss
+ --do_map_loss
