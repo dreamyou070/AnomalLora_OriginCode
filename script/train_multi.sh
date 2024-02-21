869a2af4e0c8ecb80fba465c7b2cbbd0381d6fe9
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-21 16:16:36 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -8,7 +8,7 @@ bench_mark='MVTec3D-AD'
 sub_folder="sub_3_background_masked_sample_anomal_sample"
 folder_name="attn_loss_original_normalized_score_map_loss"
 output_dir="../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}"
-
+network_weights="../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}/epoch-000004.safetensors"
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train_multi.py \
  --log_with wandb \
@@ -16,6 +16,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --pretrained_model_name_or_path "${pretrained_model_name_or_path}" \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" --beta_scale_factor 0.8 \
  --anomal_source_path "../../../MyData/anomal_source" \
+ --network_weights "${network_weights}" \
  --anomal_only_on_object \
  --bgrm_test \
  --trigger_word "${trigger_word}" --obj_name "${obj_name}" \
