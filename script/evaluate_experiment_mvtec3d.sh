dc4e64af0a22bad04af38b941f3585b9c5fbfc07
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-21 23:37:28 +0900

    Update evaluate_experiment_mvtec3d.sh

--- script/evaluate_experiment_mvtec3d.sh
+++ script/evaluate_experiment_mvtec3d.sh
@@ -4,8 +4,8 @@ class_name="carrot"
 bench_mark="MVTec3D-AD"
 dataset_dir="../../../MyData/anomaly_detection/${bench_mark}"
 
-sub_folder="sub_3_background_masked_sample_anomal_sample"
-folder_name="attn_loss_original_normalized_score_map_loss_noise_predicting_task_loss"
+sub_folder="sub_3_background_masked_sample_anomal_sample_up_16_32_64"
+folder_name="attn_loss_original_normalized_score_map_loss"
 
 output_dir="metrics"
 
