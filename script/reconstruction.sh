5e1bed44a009070ff756a117c204113f758c31bc
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-22 14:24:11 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,10 +1,10 @@
 # !/bin/bash
 
 port_number=50022
-obj_name='cable_gland'
-caption='cable'
-sub_folder="sub_3_background_masked_sample_anomal_sample_up_16_32_64"
-folder_name="attn_loss_original_normalized_score_map_loss_noise_predicting_task_loss"
+obj_name='cookie'
+caption='cookie'
+sub_folder="sub_3_background_masked_sample_anomal_sample"
+folder_name="attn_loss_original_normalized_score_map_loss_dist_loss_normalized"
 bench_mark="MVTec3D-AD"
 position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 
@@ -14,9 +14,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --network_dim 64 --network_alpha 4 --network_folder "../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}/models" \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}/${obj_name}/test" \
  --obj_name "${obj_name}" --prompt "${caption}" \
- --latent_res 64 --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2',
-                                    'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
-                                    'up_blocks_1_attentions_2_transformer_blocks_0_attn2']" \
+ --latent_res 64 --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --d_dim 320 --use_position_embedder --position_embedding_layer ${position_embedding_layer} \
  --threds [0.55,0.6,0.65,0.7,0.75,0.8]
 
