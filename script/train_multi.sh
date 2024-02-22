a35bcdb71b335fc8ad91dedc6fbc4d161e085024
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-22 13:01:50 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -1,9 +1,9 @@
 # !/bin/bash
 
-port_number=50018
+port_number=50009
 pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
-obj_name='potato'
-trigger_word='potato'
+obj_name='rope'
+trigger_word='rope'
 bench_mark='MVTec3D-AD'
 
 
