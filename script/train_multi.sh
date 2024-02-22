d3c547debf38399ff8218ea9ed47512f75731915
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-22 22:37:33 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -1,16 +1,16 @@
 # !/bin/bash
 
-port_number=50303
+port_number=50102
 pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
-obj_name='carrot'
-trigger_word='carrot'
+obj_name='bagel'
+trigger_word='bagel'
 bench_mark='MVTec3D-AD'
 # \
 sub_folder="sub_3_background_masked_sample_anomal_sample_up_16_32_64"
 folder_name="attn_loss_original_normalized_score_map_loss_dist_loss_on_object_normalize_task_loss"
 output_dir="../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}"
 #--do_dist_loss --mahalanobis_only_object --mahalanobis_normalize \
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
  --main_process_port $port_number ../train_multi.py \
  --log_with wandb \
  --output_dir ${output_dir} \
@@ -18,7 +18,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" --beta_scale_factor 0.8 \
  --anomal_source_path "../../../MyData/anomal_source" \
  --anomal_only_on_object \
- --anomal_p 0.04 \
+ --anomal_p 0.02 \
  --bgrm_test \
  --trigger_word "${trigger_word}" --obj_name "${obj_name}" \
  --train_unet --train_text_encoder --d_dim 320 --latent_res 64 \
