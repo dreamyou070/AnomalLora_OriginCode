424fd53f2fde19c2c86b819e407c1cd4dbb74ed0
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-23 08:19:47 +0900

    Update get_all_metrics.sh

--- script/get_all_metrics.sh
+++ script/get_all_metrics.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 bench_mark="MVTec3D-AD"
-class_name="carrot"
+class_name="bagel"
 sub_folder="sub_3_background_masked_sample_anomal_sample_up_16_32_64"
-folder_name="attn_loss_original_normalized_score_map_loss_noise_predicting_task_loss"
-
+folder_name="attn_loss_original_normalized_score_map_loss"
 
 
 python ../evaluation/get_all_metrics.py \
