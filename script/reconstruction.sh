6dff13ff6110d30e16dd960c08da9c3931faf10d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-22 13:06:01 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,10 +1,10 @@
 # !/bin/bash
 
 port_number=50033
-obj_name='carrot'
-caption='carrot'
+obj_name='cable_gland'
+caption='cable'
 sub_folder="sub_3_background_masked_sample_anomal_sample"
-folder_name="attn_loss_original_normalized_score_map_loss_normalizing_mahal_feat"
+folder_name="attn_loss_original_normalized_score_map_loss_dist_loss_normalized"
 bench_mark="MVTec3D-AD"
 position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 
