8f77caff18744416ac69ef3dd121dea7635a84c6
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-22 21:31:40 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -1,15 +1,16 @@
 # !/bin/bash
 
-port_number=50303
+port_number=50203
 pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
-obj_name='ccable_gland'
+obj_name='cable_gland'
 trigger_word='cable'
 bench_mark='MVTec3D-AD'
 # \
 sub_folder="sub_3_background_masked_sample_anomal_sample_up_16_32_64"
-folder_name="attn_loss_original_normalized_score_map_loss_dist_loss_on_object_normalize_task_loss"
+folder_name="attn_loss_original_normalized_score_map_loss_noise_predicting_task_loss"
 output_dir="../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}"
-
+network_weights="../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}/models/epoch-000012.safetensors"
+#--do_dist_loss --mahalanobis_only_object --mahalanobis_normalize \
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
  --main_process_port $port_number ../train_multi.py \
  --log_with wandb \
@@ -23,13 +24,13 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
  --trigger_word "${trigger_word}" --obj_name "${obj_name}" \
  --train_unet --train_text_encoder --d_dim 320 --latent_res 64 \
  --network_dim 64 --network_alpha 4 \
+ --network_weights "${network_weights}" \
  --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_1_attentions_2_transformer_blocks_0_attn2']" \
- --start_epoch 0 --max_train_epochs 30 \
+ --start_epoch 12 --max_train_epochs 30 \
  --do_anomal_sample --do_background_masked_sample \
- --do_dist_loss --mahalanobis_only_object --mahalanobis_normalize \
  --do_attn_loss --do_normalized_score --original_normalized_score \
  --do_map_loss \
  --test_noise_predicting_task_loss
