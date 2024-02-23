b8e42df7c4b381637bd7126c2b2e057d329add17
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-23 10:44:52 +0900

    Update get_all_metrics.sh

--- script/get_all_metrics.sh
+++ script/get_all_metrics.sh
@@ -3,7 +3,7 @@
 bench_mark="MVTec3D-AD"
 class_name="cable_gland"
 sub_folder="sub_3_background_masked_sample_anomal_sample_up_16_32_64"
-folder_name="attn_loss_original_normalized_score_map_loss_noise_predicting_task_loss"
+folder_name="attn_loss_original_normalized_score_map_loss_dist_loss_on_object_normalize_task_loss"
 #folder_name="attn_loss_original_normalized_score_map_loss"
 
 
