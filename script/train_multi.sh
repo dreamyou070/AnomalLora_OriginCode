dbc66ea769937fd0756a5803c55332c0fdb3179c
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-24 07:30:34 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -5,13 +5,13 @@ obj_name='cookie'
 trigger_word='cookie'
 bench_mark='MVTec3D-AD'
 
-sub_folder="sub_3_up_16_64"
+sub_folder="sub_3_up_16_1_64_1"
 folder_name="back_noise_use_perlin_zero_timestep"
 output_dir="../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}"
 #--use_noise_scheduler --min_timestep 399 --max_timestep 400 \
 #--back_noise_use_gaussian \
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_config \
  --main_process_port $port_number ../train_multi.py \
  --log_with wandb \
  --output_dir ${output_dir} \
@@ -25,8 +25,8 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --train_unet --train_text_encoder --d_dim 320 --latent_res 64 \
  --network_dim 64 --network_alpha 4 \
  --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' \
- --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2',
-                    'up_blocks_1_attentions_2_transformer_blocks_0_attn2',]" \
+ --trg_layer_list "['up_blocks_3_attentions_0_transformer_blocks_0_attn2',
+                    'up_blocks_1_attentions_0_transformer_blocks_0_attn2',]" \
  --start_epoch 0 --max_train_epochs 30 \
  --do_anomal_sample --do_background_masked_sample \
  --do_dist_loss --mahalanobis_only_object --mahalanobis_normalize \
