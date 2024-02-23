0470e87057acb4a9a5dfe491efcb77a45dc7b362
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-23 09:27:14 +0900

    Update get_all_metrics.sh

--- script/get_all_metrics.sh
+++ script/get_all_metrics.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 bench_mark="MVTec3D-AD"
-class_name="cable_gland"
+class_name="cookie"
 sub_folder="sub_3_background_masked_sample_anomal_sample_up_16_32_64"
 #folder_name="attn_loss_original_normalized_score_map_loss_noise_predicting_task_loss"
 folder_name="attn_loss_original_normalized_score_map_loss"
