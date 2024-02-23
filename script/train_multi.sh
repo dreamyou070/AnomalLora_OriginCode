659a8b10f79897146136649adf169ee949a7b8e4
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-24 07:16:04 +0900

    train1

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -11,7 +11,7 @@ output_dir="../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}"
 #--use_noise_scheduler --min_timestep 399 --max_timestep 400 \
 #--back_noise_use_gaussian \
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train_multi.py \
  --log_with wandb \
  --output_dir ${output_dir} \
