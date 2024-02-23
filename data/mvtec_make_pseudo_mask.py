8c9aa2cdeb8265a3f4b79f91a7a619f7c179d616
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-23 13:02:05 +0900

    Update mvtec_make_pseudo_mask.py

--- data/mvtec_make_pseudo_mask.py
+++ data/mvtec_make_pseudo_mask.py
@@ -54,6 +54,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--base_folder', type=str,
                         default=r'/home/dreamyou070/MyData/anomaly_detection/MVTec')
-    parser.add_argument('--trg_cat', type=str, default='carpet')
+    parser.add_argument('--trg_cat', type=str, default='leather')
     args = parser.parse_args()
     main(args)
