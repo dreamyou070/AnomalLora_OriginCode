8f11f44e40787437c33b63e60e3fe334ed77600c
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-23 22:57:26 +0900

    Update evaluate_experiment_mvtec3d.sh

--- script/evaluate_experiment_mvtec3d.sh
+++ script/evaluate_experiment_mvtec3d.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 bench_mark="MVTec3D-AD"
-class_name="bagel"
+obj_name='cookie'
 dataset_dir="../../../MyData/anomaly_detection/${bench_mark}"
 
-sub_folder="sub_3_background_masked_sample_anomal_sample_up_16_32_64"
-folder_name="attn_loss_original_normalized_score_map_loss_dist_loss_on_object_normalize_task_loss"
+sub_folder="sub_3_background_masked_sample_anomal_sample_up_16_0_2_32_64"
+folder_name="attn_loss_original_normalized_score_map_loss_dist_loss_on_object_normalize_task_loss_back_noise_perlin_shape"
 output_dir="metrics"
 
 python ../evaluation/evaluation_code_MVTec3D-AD/evaluate_experiment.py \
