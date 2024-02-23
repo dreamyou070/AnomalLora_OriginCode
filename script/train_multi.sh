d76c2bd71774038024c6ec7131861d13a19cc793
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-23 16:48:53 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -1,17 +1,15 @@
 # !/bin/bash
-
-port_number=59223
+port_number=59224
 pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
 obj_name='cable'
 trigger_word='cable'
 bench_mark='MVTec'
 # --do_dist_loss --mahalanobis_only_object --mahalanobis_normalize \
-
-sub_folder="sub_3_background_masked_sample_anomal_sample_up_16_32_64"
+sub_folder="sub_3_background_masked_sample_anomal_sample_up_16_0_2_32_64"
 folder_name="attn_loss_original_normalized_score_map_loss_dist_loss_on_object_normalize_task_loss"
 output_dir="../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}"
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_config \
  --main_process_port $port_number ../train_multi.py \
  --log_with wandb \
  --output_dir ${output_dir} \
@@ -27,7 +25,8 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config \
  --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
-                    'up_blocks_1_attentions_2_transformer_blocks_0_attn2']" \
+                    'up_blocks_1_attentions_2_transformer_blocks_0_attn2',
+                    'up_blocks_1_attentions_0_transformer_blocks_0_attn2']" \
  --start_epoch 0 --max_train_epochs 30 \
  --do_anomal_sample --do_background_masked_sample \
  --do_dist_loss --mahalanobis_only_object --mahalanobis_normalize \
