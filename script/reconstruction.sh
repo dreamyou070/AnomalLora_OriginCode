8947095657ef64ba83e95572afbaaef1bd1f4975
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-23 21:37:57 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,8 +1,8 @@
 # !/bin/bash
 
-port_number=50903
-obj_name='bagel'
-caption='bagel'
+port_number=50503
+obj_name='potato'
+caption='potato'
 sub_folder="sub_3_background_masked_sample_anomal_sample_up_16_32_64"
 folder_name="attn_loss_original_normalized_score_map_loss_dist_loss_on_object_normalize_task_loss"
 bench_mark="MVTec3D-AD"
@@ -18,4 +18,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
                                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                                     'up_blocks_1_attentions_2_transformer_blocks_0_attn2',]" \
  --d_dim 320 --use_position_embedder --position_embedding_layer ${position_embedding_layer} \
- --threds [0.7,0.75,0.8,0.85,0.9,0.95,0.98]
+ --threds [0.5]
