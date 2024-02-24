b5d371a3153cdec1b32a2a10b6d5d875666177ff
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-25 06:57:42 +0900

    Update evaluate_experiment_mvtec3d.sh

--- script/evaluate_experiment_mvtec3d.sh
+++ script/evaluate_experiment_mvtec3d.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 bench_mark="MVTec3D-AD"
-class_name="cookie"
+class_name="dowel"
 dataset_dir="../../../MyData/anomaly_detection/${bench_mark}"
 layer_folder="layer_3"
 sub_folder="sub_3_up_16_32_64"
-folder_name="zero_timestep_sigma_max_100_min_sigma_30_max_perlin_scale_4_max_beta_scale_0.95_min_beta_scale_0.6"
+folder_name="zero_timestep_sigma_max_60_min_sigma_25_max_perlin_scale_6"
 output_dir="metrics"
 
 
