e7e50461aa115299c3d724420a4e71d03145797d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-24 22:29:49 +0900

    Update get_all_metrics.sh

--- script/get_all_metrics.sh
+++ script/get_all_metrics.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 
 bench_mark="MVTec3D-AD"
-class_name="foam"
+class_name="peach"
 layer_folder="layer_3"
 sub_folder="background_masked_sample_anomal_sample_up_16_32_64"
 folder_name="attn_loss_original_normalized_score_map_loss_dist_loss_on_object_normalize_task_loss"
-
 
 python ../evaluation/get_all_metrics.py \
   --class_name ${class_name} \
