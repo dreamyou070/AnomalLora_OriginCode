c7b5c3670918e329b0dfc24a5d537e92aed9002b
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-24 13:32:57 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -1,18 +1,19 @@
 # !/bin/bash
-port_number=50202
+port_number=50111
 pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
-obj_name='dowel'
-trigger_word='dowel'
+obj_name='cookie'
+trigger_word='cookie'
 bench_mark='MVTec3D-AD'
 
 layer_folder="layer_3"
 sub_folder="sub_3_up_16_32_64"
-folder_name="back_noise_use_perlin_timestep_zero_to_200"
+folder_name="back_noise_use_gaussian_timestep_zero_to_400"
 output_dir="../../result/${bench_mark}/${obj_name}/${layer_folder}/${sub_folder}/${folder_name}"
+network_weights="../../result/${bench_mark}/${obj_name}/${layer_folder}/${sub_folder}/${folder_name}/models/epoch-000009.safetensors"
 #--use_noise_scheduler --min_timestep 399 --max_timestep 400 \
 #--back_noise_use_gaussian \
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
  --main_process_port $port_number ../train_multi.py \
  --log_with wandb \
  --output_dir ${output_dir} \
@@ -29,9 +30,11 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_config \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_1_attentions_2_transformer_blocks_0_attn2',]" \
- --start_epoch 0 --max_train_epochs 30 \
+ --network_weights "${network_weights}" \
+ --start_epoch 9 --max_train_epochs 30 \
  --do_anomal_sample --do_background_masked_sample \
  --do_dist_loss --mahalanobis_only_object --mahalanobis_normalize \
  --do_attn_loss --do_normalized_score --original_normalized_score \
  --do_map_loss \
- --use_noise_scheduler --min_timestep 0 --max_timestep 200
+ --use_noise_scheduler --min_timestep 0 --max_timestep 400 \
+ --back_noise_use_gaussian
