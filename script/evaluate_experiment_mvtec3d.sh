a73c7a7083645e71a16e28015eee2e16a06e4f28
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-24 20:34:25 +0900

    Update evaluate_experiment_mvtec3d.sh

--- script/evaluate_experiment_mvtec3d.sh
+++ script/evaluate_experiment_mvtec3d.sh
@@ -8,6 +8,7 @@ sub_folder="background_masked_sample_anomal_sample_up_16_32_64"
 folder_name="attn_loss_original_normalized_score_map_loss_dist_loss_on_object_normalize_task_loss"
 output_dir="metrics"
 
+
 python ../evaluation/evaluation_code_MVTec3D-AD/evaluate_experiment.py \
      --base_dir "../../result/${bench_mark}/${class_name}/${layer_folder}/${sub_folder}/${folder_name}/reconstruction" \
      --dataset_base_dir "${dataset_dir}" \
