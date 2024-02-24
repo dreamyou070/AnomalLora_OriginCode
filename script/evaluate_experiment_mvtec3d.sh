fe27035fafc386e9246cd7307d6acdf200cd7ecf
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-24 23:15:26 +0900

    Update evaluate_experiment_mvtec3d.sh

--- script/evaluate_experiment_mvtec3d.sh
+++ script/evaluate_experiment_mvtec3d.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 bench_mark="MVTec3D-AD"
-class_name="peach"
+class_name="cookie"
 dataset_dir="../../../MyData/anomaly_detection/${bench_mark}"
 layer_folder="layer_3"
-sub_folder="background_masked_sample_anomal_sample_up_16_32_64"
-folder_name="attn_loss_original_normalized_score_map_loss_dist_loss_on_object_normalize_task_loss"
+sub_folder="sub_3_up_16_32_64"
+folder_name="zero_timestep_sigma_max_100_min_sigma_30_max_perlin_scale_4"
 output_dir="metrics"
 
 
