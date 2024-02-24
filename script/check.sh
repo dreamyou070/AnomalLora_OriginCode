f2cd5e522d087b2dc5c12cb88093be6a65035f89
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-24 15:42:27 +0900

    Update check.sh

--- script/check.sh
+++ script/check.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
-port_number=50345
+port_number=50355
 pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
 obj_name='cookie'
 trigger_word='cookie'
@@ -7,12 +7,12 @@ bench_mark='MVTec3D-AD'
 
 layer_folder="layer_3"
 sub_folder="sub_3_up_16_32_64"
-folder_name="test"
+folder_name="back_noise_use_gaussian_timestep_zero_to_400_with_text_timeembedding"
 output_dir="../../result/${bench_mark}/${obj_name}/${layer_folder}/${sub_folder}/${folder_name}"
 #--use_noise_scheduler --min_timestep 399 --max_timestep 400 \
 #
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../check.py \
  --log_with wandb \
  --output_dir ${output_dir} \
@@ -29,7 +29,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_1_attentions_2_transformer_blocks_0_attn2',]" \
- --start_epoch 10 --max_train_epochs 20 \
+ --start_epoch 0 --max_train_epochs 30 \
  --do_anomal_sample --do_background_masked_sample \
  --do_dist_loss --mahalanobis_only_object --mahalanobis_normalize \
  --do_attn_loss --do_normalized_score --original_normalized_score \
