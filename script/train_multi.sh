0889c9835b4aac383ff8b8802842447d597aec7f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-25 07:16:38 +0900

    train bottle

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -1,13 +1,13 @@
 # !/bin/bash
-port_number=50007
+port_number=50111
 pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
-obj_name='cookie'
-trigger_word='peach'
-bench_mark='MVTec3D-AD'
+obj_name='bottle'
+trigger_word='bottle'
+bench_mark='MVTec'
 
 layer_folder="layer_3"
-sub_folder="sub_3_up_16_32_64"
-folder_name="zero_timestep_sigma_max_100_min_sigma_30_max_perlin_scale_4_max_beta_scale_0.95_min_beta_scale_0.6"
+sub_folder="up_16_32_64"
+folder_name="zero_timestep_sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.8_min_beta_scale_0.5"
 output_dir="../../result/${bench_mark}/${obj_name}/${layer_folder}/${sub_folder}/${folder_name}"
 # --use_noise_scheduler --min_timestep 399 --max_timestep 400 \
 # --use_text_time_embedding
@@ -21,7 +21,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" --beta_scale_factor 1.0 \
  --anomal_source_path "../../../MyData/anomal_source" \
  --anomal_only_on_object \
- --anomal_p 0.03 \
+ --anomal_p 0.04 \
  --bgrm_test \
  --trigger_word "${trigger_word}" --obj_name "${obj_name}" \
  --train_unet --train_text_encoder --d_dim 320 --latent_res 64 \
@@ -34,6 +34,6 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --do_anomal_sample --do_background_masked_sample \
  --do_attn_loss \
  --do_map_loss \
- --back_noise_use_gaussian --max_sigma 100 --min_sigma 30 --max_perlin_scale 4 \
+ --back_noise_use_gaussian --max_sigma 60 --min_sigma 25 --max_perlin_scale 6 \
  --use_noise_scheduler --min_timestep 0 --max_timestep 1 \
- --max_beta_scale 0.95 --min_beta_scale 0.6
+ --max_beta_scale 0.8 --min_beta_scale 0.5
