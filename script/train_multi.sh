1a51c4a2916b222dabf60d0eda0ea343aa4cd3ee
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-24 21:08:45 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -1,19 +1,19 @@
 # !/bin/bash
-port_number=50055
+port_number=50004
 pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
-obj_name='dowel'
-trigger_word='dowel'
+obj_name='cookie'
+trigger_word='cookie'
 bench_mark='MVTec3D-AD'
 
 layer_folder="layer_3"
 sub_folder="sub_3_up_16_32_64"
-folder_name="zero_timestep_sigma_max_60_min_sigma_25_max_perlin_scale_6"
+folder_name="zero_timestep_sigma_max_100_min_sigma_30_max_perlin_scale_4"
 output_dir="../../result/${bench_mark}/${obj_name}/${layer_folder}/${sub_folder}/${folder_name}"
 # --use_noise_scheduler --min_timestep 399 --max_timestep 400 \
 # --use_text_time_embedding
 # --do_dist_loss --mahalanobis_only_object --mahalanobis_normalize --dist_loss_with_max \
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train_multi.py \
  --log_with wandb \
  --output_dir ${output_dir} \
@@ -21,7 +21,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_config \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" --beta_scale_factor 0.8 \
  --anomal_source_path "../../../MyData/anomal_source" \
  --anomal_only_on_object \
- --anomal_p 0.04 \
+ --anomal_p 0.03 \
  --bgrm_test \
  --trigger_word "${trigger_word}" --obj_name "${obj_name}" \
  --train_unet --train_text_encoder --d_dim 320 --latent_res 64 \
@@ -34,5 +34,5 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_config \
  --do_anomal_sample --do_background_masked_sample \
  --do_attn_loss \
  --do_map_loss \
- --back_noise_use_gaussian --max_sigma 60 --min_sigma 25 --max_perlin_scale 6 \
+ --back_noise_use_gaussian --max_sigma 100 --min_sigma 30 --max_perlin_scale 4 \
  --use_noise_scheduler --min_timestep 0 --max_timestep 1
