cfdfd2515f803a52d22569f1f4c9bcb86b3f5869
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-24 23:12:19 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,11 +1,11 @@
 # !/bin/bash
 
-port_number=50061
-obj_name='peach'
-caption='peach'
+port_number=50001
+obj_name='cookie'
+caption='cookie'
 layer_folder="layer_3"
-sub_folder="background_masked_sample_anomal_sample_up_16_32_64"
-folder_name="attn_loss_original_normalized_score_map_loss_dist_loss_on_object_normalize_task_loss"
+sub_folder="sub_3_up_16_32_64"
+folder_name="zero_timestep_sigma_max_100_min_sigma_30_max_perlin_scale_4"
 bench_mark="MVTec3D-AD"
 position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 
