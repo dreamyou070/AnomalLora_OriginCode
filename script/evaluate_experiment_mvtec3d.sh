52146fb955328ed3007f92519f6728969686fe0c
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-25 08:21:46 +0900

    Update evaluate_experiment_mvtec3d.sh

--- script/evaluate_experiment_mvtec3d.sh
+++ script/evaluate_experiment_mvtec3d.sh
@@ -5,12 +5,12 @@ class_name="dowel"
 dataset_dir="../../../MyData/anomaly_detection/${bench_mark}"
 layer_folder="layer_3"
 sub_folder="sub_3_up_16_32_64"
-folder_name="zero_timestep_sigma_max_60_min_sigma_25_max_perlin_scale_6"
+folder_name="back_noise_use_gaussian_timestep_zero_to_500_with_text_timeembedding_dist_loss_with_max"
 output_dir="metrics"
 
 
 python ../evaluation/evaluation_code_MVTec3D-AD/evaluate_experiment.py \
-     --base_dir "../../result/${bench_mark}/${class_name}/${layer_folder}/${sub_folder}/${folder_name}/reconstruction" \
+     --base_dir "../../result/${bench_mark}/${class_name}/${layer_folder}/${sub_folder}/${folder_name}/reconstruction_timestep_500" \
      --dataset_base_dir "${dataset_dir}" \
      --anomaly_maps_dir "${base_dir}" \
      --output_dir "${output_dir}" \
