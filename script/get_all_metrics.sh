3996e67c439b21370698171fc104d27ec23af458
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-24 11:04:15 +0900

    Update get_all_metrics.sh

--- script/get_all_metrics.sh
+++ script/get_all_metrics.sh
@@ -2,11 +2,10 @@
 
 bench_mark="MVTec3D-AD"
 class_name="cookie"
-sub_folder="up_16_0_2_32_64"
-folder_name="back_noise_use_gaussian_400_timestep"
+layer_folder="layer_3"
+sub_folder="sub_3_up_16_32_64"
+folder_name="back_noise_use_gaussian_timestep_zero_to_200"
 
 python ../evaluation/get_all_metrics.py \
   --class_name ${class_name} \
-  --base_save_dir "../../result/${bench_mark}/${class_name}/layer_4/${sub_folder}/${folder_name}"
-
-
+  --base_save_dir "../../result/${bench_mark}/${class_name}/${layer_folder}/${sub_folder}/${folder_name}"
