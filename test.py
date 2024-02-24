03b260fd70e84aecc21f098e66d7258a23e8b036
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-24 10:42:49 +0900

    code trimming

--- test.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-resized_attn_scores = [torch.randn(8, 4096, 77), torch.randn(8, 4096, 77),torch.randn(8, 4096, 77)]
-concat_attn_score = torch.cat(resized_attn_scores, dim=0)  # 8, 4096, sen_len ***
-    #self.resized_attn_scores = []
-    #return concat_attn_score[:, :, :2]
-print(concat_attn_score.shape)
