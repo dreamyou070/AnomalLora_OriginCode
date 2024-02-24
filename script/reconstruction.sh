7eb6799c554d7967e35cfc602ae9ab0ae41af380
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-24 09:02:34 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,9 +1,9 @@
 # !/bin/bash
 
-port_number=50102
+port_number=50202
 obj_name='cookie'
 caption='cookie'
-sub_folder="sub_3_up_16_0_64_2"
+sub_folder="sub_3_up_16_1_64_1"
 folder_name="back_noise_use_perlin_zero_timestep"
 bench_mark="MVTec3D-AD"
 position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
@@ -14,7 +14,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --network_dim 64 --network_alpha 4 --network_folder "../../result/${bench_mark}/${obj_name}/${sub_folder}/${folder_name}/models" \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}/${obj_name}/test" \
  --obj_name "${obj_name}" --prompt "${caption}" \
- --latent_res 64 --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2',
+ --latent_res 64 --trg_layer_list "['up_blocks_3_attentions_0_transformer_blocks_0_attn2',
                                     'up_blocks_1_attentions_0_transformer_blocks_0_attn2',]" \
  --d_dim 320 --use_position_embedder --position_embedding_layer ${position_embedding_layer} \
  --threds [0.5]
