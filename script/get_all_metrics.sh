4b6f1703201c25d4326e2eee2b2afe60cf9e4963
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-26 07:07:58 +0900

    Update get_all_metrics.sh

--- script/get_all_metrics.sh
+++ script/get_all_metrics.sh
@@ -3,7 +3,7 @@
 bench_mark="MVTec3D-AD"
 class_name="leather"
 layer_folder="layer_3"
-sub_folder="sub_3_up_16_32_64"
+sub_folder="up_16_32_64"
 folder_name="sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.8_min_beta_scale_0.5_back_perlin"
 
 python ../evaluation/get_all_metrics.py \
