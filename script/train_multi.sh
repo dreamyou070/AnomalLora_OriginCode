a183c3cbd5a6e937cfb0f03db72c5970ddcecb04
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-25 10:46:04 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -1,18 +1,18 @@
 # !/bin/bash
-port_number=50116
+port_number=50101
 pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
-obj_name='dowel'
-trigger_word='dowel'
+obj_name='bottle'
+trigger_word='bottle'
 bench_mark='MVTec3D-AD'
 
 layer_folder="layer_3"
 sub_folder="up_16_32_64"
-folder_name="zero_timestep_sigma_max_60_min_sigma_25_max_perlin_scale_6_test_noise_predicting_task_loss_do_cls_train_dist_loss_diff_sigma_scale"
+folder_name="zero_timestep_sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.8_min_beta_scale_0.5_back_perlin"
 output_dir="../../result/${bench_mark}/${obj_name}/${layer_folder}/${sub_folder}/${folder_name}"
 # --use_noise_scheduler --min_timestep 399 --max_timestep 400 \
 # --use_text_time_embedding
 # --do_dist_loss --mahalanobis_only_object --mahalanobis_normalize --dist_loss_with_max \
-#
+# --test_noise_predicting_task_loss \
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train_multi.py \
@@ -33,9 +33,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
                     'up_blocks_1_attentions_2_transformer_blocks_0_attn2',]" \
  --start_epoch 0 --max_train_epochs 30 \
  --do_anomal_sample --do_background_masked_sample \
- --do_dist_loss --mahalanobis_only_object --mahalanobis_normalize --dist_loss_with_max \
- --do_attn_loss --do_cls_train \
+ --do_attn_loss \
  --do_map_loss \
- --test_noise_predicting_task_loss \
- --back_noise_use_gaussian --max_sigma 60 --min_sigma 25 --max_perlin_scale 6 \
- --max_beta_scale 1 --min_beta_scale 0
+ --max_sigma 60 --min_sigma 25 --max_perlin_scale 6 \
+ --max_beta_scale 0.8 --min_beta_scale 0.5
