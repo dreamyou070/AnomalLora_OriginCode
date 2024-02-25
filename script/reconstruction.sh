e41d853612809c58d0ab9c72beab7135f423209a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-25 23:20:17 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,8 +1,8 @@
 # !/bin/bash
 
-port_number=50012
-obj_name='capsule'
-caption='capsule'
+port_number=50013
+obj_name='hazelnut'
+caption='hazelnut'
 
 layer_folder="layer_3"
 sub_folder="up_16_32_64"
