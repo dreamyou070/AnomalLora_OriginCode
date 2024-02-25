2512c36d4928a9d1e60371c3135d1a74b9f7c4c8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-26 07:07:34 +0900

    Update get_all_metrics.sh

--- script/get_all_metrics.sh
+++ script/get_all_metrics.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 bench_mark="MVTec3D-AD"
-class_name="dowel"
+class_name="leather"
 layer_folder="layer_3"
 sub_folder="sub_3_up_16_32_64"
-folder_name="zero_timestep_sigma_max_60_min_sigma_25_max_perlin_scale_6"
+folder_name="sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.8_min_beta_scale_0.5_back_perlin"
 
 python ../evaluation/get_all_metrics.py \
   --class_name ${class_name} \
