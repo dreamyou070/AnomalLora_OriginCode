fc2918768a595e3c0b148b473d903b27bac2299e
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-26 00:03:41 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -1,13 +1,13 @@
 # !/bin/bash
-port_number=51010
+port_number=51018
 pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
-obj_name='capsule'
-trigger_word='capsule'
+obj_name='metal_nut'
+trigger_word='metal'
 bench_mark='MVTec'
 
 layer_folder="layer_3"
 sub_folder="up_16_32_64"
-folder_name="sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.8_min_beta_scale_0.5_not_rot_aug"
+folder_name="sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.8_min_beta_scale_0.5"
 output_dir="../../result/${bench_mark}/${obj_name}/${layer_folder}/${sub_folder}/${folder_name}"
 # --use_noise_scheduler --min_timestep 399 --max_timestep 400 \
 # --use_text_time_embedding
@@ -34,4 +34,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --do_attn_loss \
  --do_map_loss \
  --max_sigma 60 --min_sigma 25 --max_perlin_scale 6 \
- --max_beta_scale 0.8 --min_beta_scale 0.5
+ --max_beta_scale 0.8 --min_beta_scale 0.5 --do_rot_augment
