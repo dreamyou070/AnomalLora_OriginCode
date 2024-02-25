ebebbc02aabd628d034a228ce44c161a25607eff
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-25 12:25:32 +0900

    Update evaluate_experiment_mvtec.sh

--- script/evaluate_experiment_mvtec.sh
+++ script/evaluate_experiment_mvtec.sh
@@ -5,7 +5,7 @@ bench_mark="MVTec"
 dataset_dir="../../../MyData/anomaly_detection/${bench_mark}"
 layer_folder="layer_3"
 sub_folder="up_16_32_64"
-folder_name="zero_timestep_sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.8_min_beta_scale_0.5_back_perlin"
+folder_name="zero_timestep_sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.8_min_beta_scale_0.5"
 base_dir="../../result/${bench_mark}/${class_name}/${layer_folder}/${sub_folder}/${folder_name}/reconstruction" \
 output_dir="metrics"
 
