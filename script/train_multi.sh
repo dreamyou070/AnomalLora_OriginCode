1f2a817e4c237d5f98b234d01fbf61eb2f4231d1
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-25 10:52:32 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -3,7 +3,7 @@ port_number=50101
 pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
 obj_name='bottle'
 trigger_word='bottle'
-bench_mark='MVTec3D-AD'
+bench_mark='MVTec'
 
 layer_folder="layer_3"
 sub_folder="up_16_32_64"
