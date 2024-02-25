3a8b3d3db4c9312bfc1ee5d1869ddd9b08dfcd1f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-25 13:09:23 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -1,8 +1,8 @@
 # !/bin/bash
-port_number=50002
+port_number=50003
 pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
-obj_name='dowel'
-trigger_word='dowel'
+obj_name='foam'
+trigger_word='foam'
 bench_mark='MVTec3D-AD'
 
 layer_folder="layer_3"
