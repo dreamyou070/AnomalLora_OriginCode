731e086cf4105f31b9e669ab92d82ef0f49f97db
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-25 23:25:58 +0900

    recon again

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,12 +1,12 @@
 # !/bin/bash
 
-port_number=50015
-obj_name='leather'
-caption='leather'
+port_number=50011
+obj_name='capsule'
+caption='capsule'
 
 layer_folder="layer_3"
 sub_folder="up_16_32_64"
-folder_name="sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.8_min_beta_scale_0.5_back_perlin"
+folder_name="sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.8_min_beta_scale_0.5_back_perlin_cropping_test_anomal_p_0.01_new_code"
 bench_mark="MVTec"
 position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 
@@ -23,4 +23,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
                                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                                     'up_blocks_1_attentions_2_transformer_blocks_0_attn2',]" \
  --d_dim 320 --use_position_embedder --position_embedding_layer ${position_embedding_layer} \
- --threds [0.5]
+ --threds [0.5] --object_crop
