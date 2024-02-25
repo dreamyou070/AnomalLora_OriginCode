2125c8229626b896b1505eb25aa701ebb8e0bc20
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-25 21:46:06 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -1,8 +1,8 @@
 # !/bin/bash
-port_number=51007
+port_number=51014
 pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
-obj_name='tile'
-trigger_word='tile'
+obj_name='wood'
+trigger_word='wood'
 bench_mark='MVTec'
 
 layer_folder="layer_3"
