28a87949cb8d22307dadb81e9b59b26ba615ce47
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-26 11:29:51 +0900

    wood eval

--- script/evaluate_experiment_mvtec.sh
+++ script/evaluate_experiment_mvtec.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 bench_mark="MVTec"
-class_name="zipper"
+class_name="wood"
 dataset_dir="../../../MyData/anomaly_detection/${bench_mark}"
 layer_folder="layer_3"
 sub_folder="up_16_32_64"
--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,8 +1,8 @@
 # !/bin/bash
 
-port_number=50013
-obj_name='transistor'
-caption='transistor'
+port_number=51414
+obj_name='wood'
+caption='wood'
 
 layer_folder="layer_3"
 sub_folder="up_16_32_64"
