255a37ff30237374e32273d1268a08cdcbd85828
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-26 10:45:23 +0900

    Update evaluate_experiment_mvtec.sh

--- script/evaluate_experiment_mvtec.sh
+++ script/evaluate_experiment_mvtec.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 bench_mark="MVTec"
-class_name="tile"
+class_name="zipper"
 dataset_dir="../../../MyData/anomaly_detection/${bench_mark}"
 layer_folder="layer_3"
 sub_folder="up_16_32_64"
-folder_name="sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.8_min_beta_scale_0.5_back_perlin_cropping_test_anomal_p_0.01_new_code"
+folder_name="sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.7_min_beta_scale_0.4_not_rot_not_backnoise"
 
 base_dir="../../result/${bench_mark}/${class_name}/${layer_folder}/${sub_folder}/${folder_name}/reconstruction" \
 output_dir="metrics"
