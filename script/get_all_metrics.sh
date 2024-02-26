52f9f61b71f004e24fa16c1dd78869e773f0a1db
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-26 11:35:42 +0900

    Update get_all_metrics.sh

--- script/get_all_metrics.sh
+++ script/get_all_metrics.sh
@@ -4,7 +4,7 @@ bench_mark="MVTec"
 class_name="wood"
 layer_folder="layer_3"
 sub_folder="up_16_32_64"
-folder_name="sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.8_min_beta_scale_0.5_back_perlin_cropping_test_anomal_p_0.01_new_code""
+folder_name="sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.8_min_beta_scale_0.5_back_perlin_cropping_test_anomal_p_0.01_new_code"
 
 python ../evaluation/get_all_metrics.py \
   --class_name ${class_name} \
