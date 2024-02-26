daee2eb78bd15c77c4809a27f53eda531cd44a77
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-26 10:29:06 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -1,13 +1,13 @@
 # !/bin/bash
-port_number=51411
+port_number=51111
 pretrained_model_name_or_path="../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors"
-obj_name='screw'
-trigger_word='screw'
+obj_name='tile'
+trigger_word='tile'
 bench_mark='MVTec'
 
 layer_folder="layer_3"
 sub_folder="up_16_32_64"
-folder_name="sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.7_min_beta_scale_0.4"
+folder_name="sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.95_min_beta_scale_0.6_rot_augment"
 output_dir="../../result/${bench_mark}/${obj_name}/${layer_folder}/${sub_folder}/${folder_name}"
 # --use_noise_scheduler --min_timestep 399 --max_timestep 400 \
 # --use_text_time_embedding
@@ -21,7 +21,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" --beta_scale_factor 1.0 \
  --anomal_source_path "../../../MyData/anomal_source" \
  --anomal_only_on_object \
- --anomal_p 0.02 \
+ --anomal_p 0.01 \
  --trigger_word "${trigger_word}" --obj_name "${obj_name}" \
  --train_unet --train_text_encoder --d_dim 320 --latent_res 64 \
  --network_dim 64 --network_alpha 4 \
@@ -34,6 +34,5 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --do_attn_loss \
  --do_map_loss \
  --do_rot_augment \
- --back_noise_use_gaussian \
  --max_sigma 60 --min_sigma 25 --max_perlin_scale 6 \
- --max_beta_scale 0.7 --min_beta_scale 0.4
+ --max_beta_scale 0.95 --min_beta_scale 0.6
