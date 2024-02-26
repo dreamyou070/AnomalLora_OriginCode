968803b0c3afceacc0417bb49ef4f217fa210058
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-26 15:56:36 +0900

    Update get_all_metrics.sh

--- script/get_all_metrics.sh
+++ script/get_all_metrics.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 bench_mark="MVTec"
-class_name="wood"
+class_name="leather"
 layer_folder="layer_3"
 sub_folder="up_16_32_64"
-folder_name="sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.7_min_beta_scale_0.4_not_rot_not_backnoise"
+folder_name="sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.8_min_beta_scale_0.5_back_perlin"
 
 python ../evaluation/get_all_metrics.py \
   --class_name ${class_name} \
