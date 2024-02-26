0d01410d4f9ae747a28c3d452fd12ca9cf3b4a23
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-26 12:55:17 +0900

    Update background_removing.py

--- data/background_removing.py
+++ data/background_removing.py
@@ -15,8 +15,8 @@ api_key = 'iuUxT3T8g4BeqasbkP5EPghZ'
 rmbg = RemoveBg(api_key, "error.log")
 for image in images:
     img_dir = os.path.join(rgb_folder, image)
-    rmbg.remove_background_from_img_file(img_dir)
-    rmbg.save(os.path.join(object_folder, image))
+    rmbg.remove_background_from_img_file(img_file_path = img_dir,
+                                         new_file_name=os.path.join(object_folder, image))
     rmbg.close()
     print(f'processed {image}')
     break
