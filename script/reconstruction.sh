5558592eded105839bbfa6f189baa308353b19a8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-26 10:37:47 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,12 +1,12 @@
 # !/bin/bash
 
-port_number=50014
-obj_name='pill'
-caption='pill'
+port_number=50008
+obj_name='metal_nut'
+caption='metal'
 
 layer_folder="layer_3"
 sub_folder="up_16_32_64"
-folder_name="sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.8_min_beta_scale_0.5_not_rot_aug"
+folder_name="sigma_max_60_min_sigma_25_max_perlin_scale_6_max_beta_scale_0.8_min_beta_scale_0.5"
 bench_mark="MVTec"
 position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 
